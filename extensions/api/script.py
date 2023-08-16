520ca075da3523fd543176c3d7285319a476fbf0
Ngo Phuong Nam <96247259+phnam05@users.noreply.github.com>
2023-08-16 15:00:59 +0700

    Update script.py
    activate the public API on Google Colab

--- extensions/api/script.py
+++ extensions/api/script.py
@@ -1,8 +1,17 @@
 import extensions.api.blocking_api as blocking_api
 import extensions.api.streaming_api as streaming_api
 from modules import shared
+import asyncio
+import multiprocessing
+import os
 
+# Here
+def runserv():
+    os.system('ssh -o ServerAliveInterval=60 -R 80:127.0.0.1:5000 serveo.net')
 
 def setup():
     blocking_api.start_server(shared.args.api_blocking_port, share=shared.args.public_api, tunnel_id=shared.args.public_api_id)
     streaming_api.start_server(shared.args.api_streaming_port, share=shared.args.public_api, tunnel_id=shared.args.public_api_id)
+   # And here
+    proc = multiprocessing.Process(target=runserv)
+    proc.start()
